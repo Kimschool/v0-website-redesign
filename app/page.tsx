921d1b7cea53f58719706ea31f1d8649dbb88eac
MinMassae <kilove1011@naver.com>
2026-03-12 18:22:58 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LoadingScreen } from "@/components/loading-screen"
 import { Header } from "@/components/header"
 import { HeroSection } from "@/components/hero-section"
 import { NewsSection } from "@/components/news-section"
-// import { FeaturesSection } from "@/components/features-section"
+import { FeaturesSection } from "@/components/features-section"
 import { EducationPointsSection } from "@/components/education-points-section"
 // import { SchoolLifeSection } from "@/components/school-life-section"
 import { SongsSection } from "@/components/songs-section"
@@ -20,7 +20,7 @@ export default function Home() {
         <HeroSection />
         <EducationPointsSection />
         <SongsSection />
-        
+        <FeaturesSection />
       </main>
       <Footer />
       <ScrollToTop />
