f5785598b38dd65b21ba98c4c76bf3c3436097c8
MinMassae <kilove1011@naver.com>
2026-03-13 14:46:11 +0900

    Update features-section.tsx

--- components/features-section.tsx
+++ components/features-section.tsx
@@ -169,7 +169,7 @@ useEffect(() => {
                     </div>
                   </div>
                 </div>
-                )
+                
               ))}
             </div>
           </div>
